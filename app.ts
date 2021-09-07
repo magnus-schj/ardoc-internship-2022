58545c2ec28bb77af35e7f13b7cf875b0ab95efc
Magnus <beachmagnus+git@gmail.com>
2021-09-07 15:56:53 +0200

    task 1 completed

--- app.ts
+++ app.ts
@@ -1,1 +1,11 @@
-const sorter = () => {};
+const sorter = (numbers: number[]) => {
+  if (numbers.length < 3) return;
+  const sortedNumbers: number[] = numbers.sort((a, b) => b - a);
+  let product = 1;
+  for (let i = 0; i < 3; i++) {
+    product *= sortedNumbers[i];
+  }
+  return product;
+};
+
+console.log(sorter([1, 10, 2, 6, 5, 3]));
