56e891a67e1f64d793d46216fa0dcd7f2e7d6e2c
Magnus <beachmagnus+git@gmail.com>
2021-09-12 15:52:11 +0200

    imporved algorythm

--- task-1/app.ts
+++ task-1/app.ts
@@ -1,9 +1,19 @@
 const sorter = (numbers: number[]) => {
-  if (numbers.length < 3) return;
   const sortedNumbers: number[] = numbers.sort((a, b) => b - a);
   let product = 1;
-  for (let i = 0; i < 3; i++) {
-    product *= sortedNumbers[i];
+  if (numbers.length < 3) {
+    for (let i = 0; i < 3; i++) {
+      if (i >= sortedNumbers.length) {
+        product *= sortedNumbers[0];
+        break;
+      }
+      product *= sortedNumbers[i];
+    }
+  } else {
+    for (let i = 0; i < 3; i++) {
+      product *= sortedNumbers[i];
+    }
   }
+
   return product;
 };
